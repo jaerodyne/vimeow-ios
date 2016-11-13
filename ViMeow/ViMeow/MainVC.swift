b3ce4eaf3c61e99d10d61741418b911d3a7ce1e8
Jillian Somera <jsomera@gmail.com>
2016-11-13 00:16:27 -0600

    Create view controllers

--- ViMeow/ViMeow/MainVC.swift
+++ ViMeow/ViMeow/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  ViMeow
 //
 //  Created by Jillian Somera on 11/12/16.
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-import Alamofire
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
