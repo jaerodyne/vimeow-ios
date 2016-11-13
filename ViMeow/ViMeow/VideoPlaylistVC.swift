8f99968cef5f253822c01c3a329bdbfb6f7051ec
Jillian Somera <jsomera@gmail.com>
2016-11-13 00:38:51 -0600

    Implement tableview

--- ViMeow/ViMeow/VideoPlaylistVC.swift
+++ ViMeow/ViMeow/VideoPlaylistVC.swift
@@ -9,28 +9,33 @@
 import UIKit
 import Alamofire
 
-class VideoPlaylistVC: UIViewController {
-
+class VideoPlaylistVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var videos = [Video]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let video = videos[indexPath.row]
+        
+        performSegue(withIdentifier: "VideoVC", sender: video)
+    }
 }
