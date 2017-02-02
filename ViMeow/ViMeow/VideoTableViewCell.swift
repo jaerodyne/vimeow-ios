5ebf923743f6200f21a185245971274c1bdb9b51
Jillian Somera <jsomera@gmail.com>
2017-02-02 06:15:02 -0600

    Clean Derived Data

---
+++ ViMeow/ViMeow/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  ViMeow
+//
+//  Created by Jillian Somera on 2/1/17.
+//  Copyright © 2017 Jillian Somera. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
