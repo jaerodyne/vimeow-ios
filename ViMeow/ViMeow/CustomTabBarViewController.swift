de670f5bb02dd06b4fbb8b9f35168a5fce32ca7a
Jillian Somera <jsomera@gmail.com>
2017-02-22 11:08:22 -0600

    Adjust collection view header

--- ViMeow/ViMeow/CustomTabBarViewController.swift
+++ ViMeow/ViMeow/CustomTabBarViewController.swift
@@ -18,41 +18,6 @@ class CustomTabBarViewController: UITabBarController, UITabBarControllerDelegate
         
         self.delegate = self
         
-        if let items = self.tabBar.items {
-            
-            //Get the height of the tab bar
-            
-            let height = self.tabBar.bounds.height
-            
-            //Calculate the size of the items
-            
-            let numItems = CGFloat(items.count)
-            let itemSize = CGSize(
-                width: tabBar.frame.width / numItems,
-                height: tabBar.frame.height)
-            
-            for (index, _) in items.enumerated() {
-                
-                //We don't want a separator on the left of the first item.
-                
-                if index > 0 {
-                    
-                    //Xposition of the item
-                    
-                    let xPosition = itemSize.width * CGFloat(index)
-                    
-                    /* Create UI view at the Xposition,
-                     with a width of 0.5 and height equal
-                     to the tab bar height, and give the
-                     view a background color
-                     */
-                    let separator = UIView(frame: CGRect(
-                        x: xPosition, y: 0, width: 2.0, height: height))
-                    separator.backgroundColor = UIColor.gray
-                    tabBar.insertSubview(separator, at: 1)
-                }
-            }
-        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
